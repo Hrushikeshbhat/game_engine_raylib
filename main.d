94d03f036145b32a463faa957537565596b50770
Hrushikesh Bhat <hrushikeshbhat@gmail.com>
2023-08-07 12:57:27 +0530

    fixed program not running issue

---
+++ main.d
@@ -0,0 +1,10 @@
+obj/main.o: src/main.cpp src/FlappyBird/FlappyBird.h \
+  src/FlappyBird/../Core/Engine.h \
+  src/FlappyBird/../Core/GameBase/GameObjectBase.h \
+  /usr/local/include/raylib.h /usr/local/include/raymath.h \
+  src/FlappyBird/../Core/GameBase/../Utils/Global.h \
+  src/FlappyBird/../Core/GameBase/Scene.h \
+  src/FlappyBird/../Core/GameProgram.h \
+  src/FlappyBird/../Core/Utils/Global.h \
+  src/FlappyBird/../Core/GameBase/SceneManager.h \
+  src/FlappyBird/../Core/GameBase/Cricle.h
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,5 @@
 
 int main()
 {
-    FlappyBird program();
+    FlappyBird program;
 }
